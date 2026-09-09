c835455b0ca5540845c7875f35919f4bf0c40283
RAMESH VISHWAKARMA <disha.ramesh-vishwakarma@capgemini.com>
2026-09-09 16:33:51 +0530

    'v1'

---
+++ index.py
@@ -0,0 +1,82 @@
+print('hello everyone ')
+print("Hello World!")
+#THIS IS A SAMPLE CODE
+print("Have a good day.")
+print("Learning Python is fun!")
+Vs= 'Disha'
+bS= 'Vishwakarma'
+print(Vs+' '+bS)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = 5
+y = 10
+print(x + y)
+x = "Disha "
+y = "Ramesh "
+z = "Vishwakarma"
+print(x + y + z)
+x = "Disha "
+y = "Ramesh "
+z = "Vishwakarma "
+a = "26"
+print(x + y + z + a)
+a=1000
+b= 140
+if a<b:
+    print('a is less than b')
+elif b<a:
+    print('b less than a')
+else:
+    print('yeah')
+a=17
+if (a<18):
+    print('not eligible')
+else:
+    print('eligible')
+b=21
+if (b<18):
+    print('not eligible')
+else:
+    print('eligible')
+
+x=80
+if(x>60):
+    print('pass')
+else: 
+    print('fail')
+x=78
+if(80<x<90):
+    print('Grade B')
+elif(70<x<80): 
+    print('Grade C')
+elif(90<x<100):
+    print('Grade A')
+a=12
+b=30
+c=4
+d=7
+e=2
+print(a+b-c*d/e)
+b=int(input ('enter your first number :'))
+c=int(input ('enter your second number :'))
+p=b+c
+print(p)
+b=int(input ('enter your first number :'))
+if (b>17):
+    print('voter id')
+else:
+    print('no voter id')
+a=int(input ('enter your first number :'))
+if('70<a<80'):
+    print('Grade C')
+elif ('80<a<90'):
+    print('Grade B')
+elif ('90<a<100'):
+    print('Grade A')
+a=int(input ('enter your first number :'))
+b=int(input ('enter your second number :'))
+c=float(input ('enter your third number :'))
+p=(a+b-c*b/a)
+print(p)
